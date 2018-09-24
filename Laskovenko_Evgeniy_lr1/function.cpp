1ce468bd006a5d2d75e7fc2575a3581bad0ce2ce
laskovenko1 <laskovenko1@yandex.ru>
2018-09-24 23:57:11 +0300

    Laskovenko_Evgeniy lr1 done.

---
+++ Laskovenko_Evgeniy_lr1/function.cpp
@@ -0,0 +1,47 @@
+#include "function.hpp"
+
+using namespace std;
+
+bool const_expression(istream &is, int visual, int step=1, bool cur=false)
+{
+    char c;
+
+    if(is >> c)
+    {
+        if(!visual)
+            cout << c;
+
+        if(c>=48 && c<=57)
+        {
+            if(visual)
+            {
+                cout <<"STEP"<<'{'<<step<<"}: "<<c<<" IS DIGIT."<<endl;
+                step++;
+            }
+            cur = true;
+            cur = const_expression(is, visual, step, cur);
+        }
+        else if(cur==true && (c==42 || c==43 || c==45))
+        {
+            if(visual)
+            {
+                cout <<"STEP"<<'{'<<step<<"}: "<<c<<" IS OPERATOR."<<endl;
+                step++;
+            }
+            cur = false;
+            cur = const_expression(is, visual, step,cur);
+        }
+        else
+        {
+            if(visual)
+                cout <<"STEP"<<'{'<<step<<"}: "<<c<<" IS INVALID. Algorithm ends."<<endl;
+            cout << endl;
+            return cur = false;
+        }
+    }
+    else
+    {
+        cout << endl;
+        return cur;
+    }
+}
