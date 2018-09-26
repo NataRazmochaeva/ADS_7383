44b9f2080dee07d82980ae96a98271d88a4f5983
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2018-09-27 00:21:04 +0300

    Add files via upload

--- Levkovich_Dmitry_lr1/main.c
+++ Levkovich_Dmitry_lr1/main.c
@@ -4,90 +4,106 @@
 #include <ctype.h>
 #include <math.h>
 
-int* getToken(char*); //Получает лексему из строки
-void pars(char*, FILE*); //Точка входа анализатора
-int fSum(int*, FILE*); //Обрабатывает сложение и вычитание
-int fMulti(int*, FILE*); //Обрабатывает умножение
-int fUnary(int*); //Обработка унарных операторов
-int fAtom(int*); //Получает значение числа
+int* getToken(char*, char*); //Получает лексему из строки
+void pars(char *expr, char* token, FILE* out, int* count, int* max, int* deep, int k); //Точка входа анализатора
+int fSum(int*,char*, FILE*, char *token, int *count, int *max, int *deep, int k); //Обрабатывает сложение и вычитание
+int fMulti(int*,char*, FILE*, char *token, int *count, int *max, int *deep, int k); //Обрабатывает умножение и деление
+int fUnary(int*, char* token, int* deep, char*); //Обработка унарных операторов
+int fAtom(int*,char*, int* deep, char*); //Получает значение числа
 
-enum {Empty, Operator, Number} type;
-int count = 0;//счетчик шагов 
-int max = 0;//максимум выражения
-int deep = 0;//глубина
-char *expr; //Указатель на обрабатываемую строку
-char token[80]; //Лексема
-int k = 0;
+enum {Empty, Operator, Number, Char} type;
+enum {No, Syntax} error;
 
 int main()
 {
+    int count = 0;//счетчик шагов
+    int max = 0;//максимум выражения
+    int deep = 0;//глубина
+    char *expr=(char*)malloc(255*sizeof(char)); //Указатель на обрабатываемую строку
+    char token[80]; //Лексема
+    int k = 0;
     int run=1;
     int value=0;
-    char expr[255]; //Содержит вычисляемое выражение
+    //char expr[255]; //Содержит вычисляемое выражение
     FILE *in=fopen("input.txt", "r");
     FILE *out=fopen("output.txt", "w");
     while(run){
-    printf("Введите 1, если хотите ввести выражение из терминала, 2-из файла, 3-завершить программу:");
-   	scanf("%d", &k);
+    printf("Введите 1, если хотите ввести выражение из терминала, 2-из файла, 3-завершить программу: ");
+    scanf("%d", &k);
     switch(k){
-	    case 1:
-	        printf("Введите максимальное значение результата выражения:");
-    		scanf("%d\n", &max);
-	        fgets(expr,80, stdin);
-	        if(!*expr) 
-	        	return 0;    //Если введена пустая строка - завершить программу
-	        pars(expr, out); //Вычислить выражение
-	        value=atoi(expr);
-	        if(value>max){
-	            printf("Значение выражения(%d) больше максимума(%d). Программа завершилась на %d-ом шаге.\n", value, max, count);
-	            break;
-	        }
-	        printf("Result: %d, вложенность рекурсии %d\n", value, deep);
-	        break;
-	    case 2:
-	    	fscanf(in,"%d\n",&max);
-	        fgets(expr, 80, in);
-	        fprintf(out,"%s", expr);
-	        pars(expr, out);
-	        value=atoi(expr);
-	        if(value>max){
-	            fprintf(out ,"Значение выражения(%d) больше максимума(%d). Программа заверилась на %d-ом шаге\n\n", value, max, count);
-	            break;
-	        }
-	        fprintf(out, "Result: %d, вложенность рекурии %d\n\n", value, deep);
-	        break;
-	    case 3:
-	    	return 0;
-	    default:
-	        printf("Введен неверный символ\n");
-	        break;
-	    }
-	    count=0;
-	    deep=0;
-	    if(k==3)
-	    	run=0;
-	}
+        case 1:
+            printf("Введите максимальное значение результата выражения: ");
+            scanf("%d", &max);
+            getchar();
+            printf("Введите выражение: ");
+            fgets(expr, 255, stdin);
+            if(!*expr)
+                return 0;    //Если введена пустая строка - завершить программу
+            pars(expr, token, out, &count, &max, &deep, k); //Вычислить выражение
+            value=atoi(expr);
+            if(error==Syntax){
+            	printf("%s\n", expr);
+            }
+            if(value>max){
+                printf("Значение выражения(%d) больше максимума(%d). Программа завершилась на %d-ом шаге.\n", value, max, count);
+                break;
+            }
+            printf("Result: %d, вложенность рекурсии %d\n\n", value, deep);
+            break;
+        case 2:
+            fscanf(in,"%d\n",&max);
+            fgets(expr, 80, in);
+            fprintf(out,"%s", expr);
+            pars(expr,token, out, &count, &max, &deep ,k);
+            value=atoi(expr);
+            if(error==Syntax){
+            	fprintf(out, "%s\n", expr);
+            }
+            if(value>max){
+                fprintf(out ,"Значение выражения(%d) больше максимума(%d). Программа заверилась на %d-ом шаге\n\n", value, max, count);
+                break;
+            }
+            fprintf(out, "Result: %d, вложенность рекурии %d\n\n", value, deep);
+            break;
+        case 3:
+            return 0;
+        default:
+            printf("Введен неверный символ\n");
+            break;
+        }
+        count=0;
+        deep=0;
+        if(k==3)
+            run=0;
+    }
     fclose(in);
     fclose(out);
     return 0;
 }
 
-void pars(char *line, FILE* out)
+void pars(char *expr,char* token, FILE* out, int* count,int* max, int* deep, int k)
 {
     int *pointer;
     int result;
-    expr=line;
-    pointer=getToken(expr);
-    fSum(&result, out);
+    error=No;
+    pointer=getToken(expr, token);
+    fSum(&result,expr, out, token, count, max, deep, k);
     *pointer=0;
-    sprintf(expr, "%d", result);
+    switch(error)
+    {
+     case No:
+        sprintf(expr, "%d", result);
+        break;
+     case Syntax:
+        strcpy(expr, "Syntax error!");
+        break;
+    }
 }
 
-int* getToken(char *expr)
+int* getToken(char *expr, char* token)
 {
-    type=Empty;
     static int i=0;
-
+    type=Empty;
     if(expr[i]=='\0') //Если конец выражения
     {
         i=0;
@@ -110,98 +126,105 @@ int* getToken(char *expr)
         token[j+1]='\0';
         type=Number;
     }
+    else if(!isdigit(expr[i])){
+        error=Syntax;
+    }
     i++;
 return &i;
 }
 
-int fSum(int *anw, FILE* out)
+int fSum(int *anw,char* expr, FILE* out, char* token, int* count, int* max, int* deep, int k)
 {
     char op;
     int temp;
-    if(fMulti(anw, out)) return 1;
+    if(fMulti(anw,expr, out, token, count, max, deep, k)) return 1;
     while((op = *token) == '+' || op == '-')
     {
-        getToken(expr);
-        fMulti(&temp, out);
+        getToken(expr, token);
+        fMulti(&temp,expr ,out, token, count, max, deep, k);
         switch(op)
         {
          case '+':
          if(k==1)
-         	printf("Вы здесь: %d + %d\n", *anw, temp);
-         if(k==2)
-         	fprintf(out, "Вы здесь: %d + %d\n", *anw, temp);
+            printf("Вы здесь: %d + %d\n", *anw, temp);
+         else if(k==2)
+            fprintf(out, "Вы здесь: %d + %d\n", *anw, temp);
             *anw += temp;
-            count ++;
-            if(*anw>max)
+            (*count) ++;
+            if(*anw>*max)
                 return -1;
             break;
          case '-':
          if(k==1)
-         	printf("Вы здесь: %d - %d\n", *anw, temp);
-         if(k==2)
-         	fprintf(out, "Вы здесь: %d - %d\n", *anw, temp);
+            printf("Вы здесь: %d - %d\n", *anw, temp);
+         else if(k==2)
+            fprintf(out, "Вы здесь: %d - %d\n", *anw, temp);
             *anw -= temp;
-            count++;
-            if(*anw>max)
+            (*count)++;
+            if(*anw>*max)
                 return -1;
             break;
         }
     }
-	deep++;
+    (*deep)++;
 return 0;
 }
 
-int fMulti(int *anw, FILE* out)
+int fMulti(int *anw,char* expr, FILE* out, char* token, int* count, int* max, int* deep, int k)
 {
     char op;
     int temp;
-    if(fUnary(anw)) return 1; //Ошибка
+    if(fUnary(anw, token, deep, expr)) return 1; //Ошибка
 
     while((op = *token) == '*')
     {
-        getToken(expr);
-        if(fUnary(&temp)) return 1; //Ошибка
+        getToken(expr, token);
+        if(fUnary(&temp, token, deep, expr)) return 1; //Ошибка
         switch(op)
         {
          case '*':
          if(k==1)
-         	printf("Вы здесь: %d * %d\n", *anw, temp);
-         if(k==2)
-         	fprintf(out,"Вы здесь: %d * %d\n", *anw, temp);
+            printf("Вы здесь: %d * %d\n", *anw, temp);
+        else if(k==2)
+            fprintf(out,"Вы здесь: %d * %d\n", *anw, temp);
             *anw *= temp;
-            count++;
-            if(*anw>max)
+            (*count)++;
+            if(*anw>*max)
                 return -1;
             break;
         }
     }
-	deep++;
+    (*deep)++;
 return 0;
 }
 
-int fUnary(int *anw)
+int fUnary(int *anw, char* token, int* deep, char* expr)
 {
     char op=0;
     if(*token == '+' || *token == '-')
     {
         op = *token;
-        getToken(expr);
+        getToken(expr,token);
     }
-    if(fAtom(anw)) return 1; //Ошибка
+    if(fAtom(anw, token, deep, expr)) return 1; //Ошибка
 
     if(op == '-') *anw = -(*anw);
-deep++;
+(*deep)++;
 return 0;
 }
 
-int fAtom(int *anw){
+int fAtom(int *anw,char* token, int* deep, char* expr){
 
     if(type==Number){
         *anw = atoi(token);
-        getToken(expr);
+        getToken(expr, token);
     }
-    else
+    else{
+        error=Syntax;
         return 1;
-    deep++;
+    }
+    (*deep)++;
 return 0;
 }
+
+
