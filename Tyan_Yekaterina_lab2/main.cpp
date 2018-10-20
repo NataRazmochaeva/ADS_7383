0a161f8f6fce73afa1209b3068069148b88426a8
User <user@Air-User.mshome.net>
2018-10-20 22:50:39 +0300

    enum was added and report was editted

--- Tyan_Yekaterina_lab2/main.cpp
+++ Tyan_Yekaterina_lab2/main.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+enum key{
+    op_found =   -2,
+    smth_wrong = -1,
+};
+
 typedef char base;
 struct s_expr;
 struct two_ptr{
@@ -25,13 +30,14 @@ typedef s_expr* lisp;
 void errors(int err) {
     switch(err) {
         case 1:
-             cerr<<"Неверный ввод."<<endl;
-             break;
+            cerr<<"Неверный ввод."<<endl;
+            break;
         case 2:
-             cerr<<"Деление на нуль."<<endl;
-             break;
+            cerr<<"Деление на нуль."<<endl;
+            break;
         case 3:
-            cerr<<"Не хвататет значений, констант или знаков. Проверьте введенные данные и повторите попытку."<<endl;
+            cerr<<"Не хвататет значений, констант или знаков.
+            Проверьте введенные данные и повторите попытку."<<endl;
             break;
         case 4:
             cerr << "Неверный ввод."<<endl;
@@ -48,11 +54,11 @@ lisp cons (const lisp h, const lisp t){
         errors(4);
         return NULL;
     }else {
-            p->tag = false;
-            p->node.pair.hd = h;
-            p->node.pair.tl = t;
-            return p;
-        }
+        p->tag = false;
+        p->node.pair.hd = h;
+        p->node.pair.tl = t;
+        return p;
+    }
 }
 
 lisp make_char_atom (const base x) {
@@ -68,46 +74,6 @@ lisp make_char_atom (const base x) {
     }
 }
 
-int read_s_expr(string array,int*ptr);
-
-void read_lisp(string array,lisp* head,int* ptr){
-    int num=0;
-    num=read_s_expr(array,ptr);
-    if(num!=-1 && num!=-2){
-        lisp p11;
-        p11=make_char_atom(array[(*ptr)-2]);
-        int num2,num3;
-        num2=read_s_expr(array,ptr);
-        num3=read_s_expr(array,ptr);
-        lisp p12;
-        if(num3==-1 || num2==-1){
-            errors(1);
-            return;
-        }
-        if(num3!=-1 && num3!=-2){
-            lisp p1,p2;
-            (*ptr)=(*ptr)-4;
-            read_lisp(array,&p1,ptr);
-            read_lisp(array,&p2,ptr);
-            p12=cons(p1,p2);
-        }
-        if(num3==-2){
-            if(num2!=-2 && num2!=-1){
-                lisp p1,p2;
-                p1=make_char_atom(array[(*ptr)-4]);
-                p2=make_char_atom(array[(*ptr)-2]);
-                p12=cons(p1,p2);
-            }
-        }
-        (*head)=cons(p11,p12);
-    }else if(num==-2 && (*ptr)!= 0){
-        (*head)=make_char_atom(array[(*ptr)-2]);
-    }else{
-        errors(1);
-        return ;
-    }
-}
-
 int read_s_expr(string array,int* ptr){
     int num=0;
     while(!isspace(array[(*ptr)]) && (*ptr)< array.length()){
@@ -117,19 +83,59 @@ int read_s_expr(string array,int* ptr){
     if((*ptr) > 2 && isspace(array[(*ptr)-3])){
         if(isdigit(array[(*ptr)-2]) || isalpha(array[(*ptr)-2]))
             return array[(*ptr)-2];
-        else if(array[(*ptr)-2] == '+' || array[(*ptr)-2]=='-' || array[(*ptr)-2]=='*' || array[(*ptr)-2]=='/')
-            return -2;
+        else if(array[(*ptr)-2] == '+' || array[(*ptr)-2]=='-' ||
+                array[(*ptr)-2]=='*' || array[(*ptr)-2]=='/')
+            return op_found;
         else
-            return -1;
+            return smth_wrong;
     }else if((*ptr) <= 2){
         if(isdigit(array[(*ptr)-2]) || isalpha(array[(*ptr)-2]))
             return array[(*ptr)-2];
-        else if(array[(*ptr)-2] == '+' || array[(*ptr)-2]=='-' || array[(*ptr)-2]=='*' || array[(*ptr)-2]=='/')
-            return -2;
+        else if(array[(*ptr)-2] == '+' || array[(*ptr)-2]=='-' ||
+                array[(*ptr)-2]=='*' || array[(*ptr)-2]=='/')
+            return op_found;
         else
-            return -1;
+            return smth_wrong;
     }else
-        return -1;
+        return smth_wrong;
+}
+
+void read_lisp(string array,lisp* head,int* ptr){
+    int num=0;
+    num=read_s_expr(array,ptr);
+    if(num!=smth_wrong && num!=op_found){
+        lisp p11;
+        p11=make_char_atom(array[(*ptr)-2]);
+        int num2,num3;
+        num2=read_s_expr(array,ptr);
+        num3=read_s_expr(array,ptr);
+        lisp p12;
+        if(num3==smth_wrong || num2==smth_wrong){
+            errors(1);
+            return;
+        }
+        if(num3!=smth_wrong && num3!=op_found){
+            lisp p1,p2;
+            (*ptr)=(*ptr)-4;
+            read_lisp(array,&p1,ptr);
+            read_lisp(array,&p2,ptr);
+            p12=cons(p1,p2);
+        }
+        if(num3==op_found){
+            if(num2!=op_found && num2!=smth_wrong){
+                lisp p1,p2;
+                p1=make_char_atom(array[(*ptr)-4]);
+                p2=make_char_atom(array[(*ptr)-2]);
+                p12=cons(p1,p2);
+            }
+        }
+        (*head)=cons(p11,p12);
+    }else if(num==op_found && (*ptr)!= 0){
+        (*head)=make_char_atom(array[(*ptr)-2]);
+    }else{
+        errors(1);
+        return ;
+    }
 }
 
 bool write_lisp(lisp head, int i,int j,bool* flag);
@@ -139,7 +145,8 @@ bool write(lisp head,int i,int j,bool* flag){
         cerr<< "Неверный ввод."<<endl;
         return false;
     }else if(head->tag==true){
-        cerr<<"Не хвататет значений, констант или знаков. Проверьте введенные данные и повторите попытку."<<endl;
+        cerr<<"Не хвататет значений, констант или знаков.
+        Проверьте введенные данные и повторите попытку."<<endl;
         return false;
     }else{
         i++;
@@ -152,13 +159,15 @@ bool write(lisp head,int i,int j,bool* flag){
 
 bool write_lisp(lisp head, int i,int j,bool* flag){
     if(head==NULL){
-        cerr<<"Не хвататет значений, констант или знаков. Проверьте введенные данные и повторите попытку."<<endl;
+        cerr<<"Не хвататет значений, констант или знаков.
+        Проверьте введенные данные и повторите попытку."<<endl;
         return false;
     }
     if(head->tag==true){
         for(int j=0;j< i;j++)
             cout<<'\t';
-        if(head->node.atom=='+' || head->node.atom=='-' || head->node.atom=='*' || head->node.atom=='/' || (isalpha(head->node.atom))){
+        if(head->node.atom=='+' || head->node.atom=='-' || head->node.atom=='*'
+           || head->node.atom=='/' || (isalpha(head->node.atom))){
             if(head->node.atom=='/' && (*flag)==true){
                 errors(2);
                 return false;
@@ -225,7 +234,7 @@ int main(){
                 if(head==NULL)
                     cerr<<"Файл пуст."<<endl;
                 if(write(head,i,j,&flag) && flag==false)
-                                    cout<< "Корректный ввод."<<endl;
+                    cout<< "Корректный ввод."<<endl;
                 destroy(head);
                 break;
             }
