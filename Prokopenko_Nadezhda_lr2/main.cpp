2c1a5d5394f48261048705783bc6d35a3ffb3b2b
ravlasov <prokopenko_nadezhda@mail.ru>
2018-10-21 21:21:28 +0300

    Prokopenko_Nadezhda_lr2

--- Prokopenko_Nadezhda_lr2/main.cpp
+++ Prokopenko_Nadezhda_lr2/main.cpp
@@ -3,13 +3,14 @@
 #include <cstdlib>
 #include <sstream>
 #include <fstream>
+#include <cstring>
+#include <cstdio>
 
 using namespace std;
 
 typedef char* base;      // базовый тип элементов (атомов)
 
 struct s_expr {
-
     bool tag; // true: atom, false: pair
     union {
         base atom;
@@ -65,13 +66,10 @@ bool isNull(const lisp s) {
 lisp head(const lisp s) { // PreCondition: not null (s)
 
     if (s != NULL) {
-
         if (!isAtom(s)) {
-
             return s->node.pair.hd;
         }
         else {
-
             cerr << "Error: Head(atom) \n";
             exit(1);
         }
@@ -84,16 +82,12 @@ lisp head(const lisp s) { // PreCondition: not null (s)
 lisp tail(const lisp s) {// PreCondition: not null (s)
 
     if (s != NULL) {
-
         if (!isAtom(s)) {
-
             return s->node.pair.tl;
         } else {
-
             exit(1);
         }
     } else {
-
         exit(1);
     }
 }
@@ -101,19 +95,13 @@ lisp tail(const lisp s) {// PreCondition: not null (s)
 lisp cons(const lisp h, const lisp t) { // PreCondition: not isAtom (t)
 
     lisp p;
-
     if (isAtom(t)) {
-
         exit(1);
     } else {
-
         p = new s_expr;
-
         if (p == NULL) {
-
             exit(1);
         } else {
-
             p->tag = false;
             p->node.pair.hd = h;
             p->node.pair.tl = t;
@@ -125,9 +113,7 @@ lisp cons(const lisp h, const lisp t) { // PreCondition: not isAtom (t)
 void destroy(lisp s) {
 
     if (s != NULL) {
-
         if (!isAtom(s)) {
-
             destroy(head(s));
             destroy(tail(s));
         }
@@ -138,11 +124,7 @@ void destroy(lisp s) {
 void read_lisp(lisp& y, istringstream &ss) {
 
     char x;
-    do {
-
-        ss >> x;
-    } while (x == ' ');
-
+    ss>>x;
     read_s_expr(x, y, ss);
 }
 
@@ -153,13 +135,16 @@ void read_s_expr(char prev, lisp& y, istringstream &ss) {
     streampos oldpos;
     base str = (base)calloc(30, sizeof(char));
     if (prev == ')') {
-        exit(1);
+      exit(1);
     } else if (prev != '(') {
-        while (ch != ' ' && ch != ')') {
+        while ( isalpha(ch) && ch != ')') {
             str[i] = ch;
             i++;
             oldpos = ss.tellg();
-            ch = ss.get();
+            ch=ss.get();
+            while (ch == ' ') {
+                ch=ss.get();
+            }
         }
         if (ch == ')') {
            ss.seekg(oldpos);
@@ -171,6 +156,7 @@ void read_s_expr(char prev, lisp& y, istringstream &ss) {
 }
 
 
+
 void read_seq(lisp& y, istringstream &ss) {
 
     char x;
@@ -178,9 +164,6 @@ void read_seq(lisp& y, istringstream &ss) {
     if (!(ss >> x)) {
         exit(1);
     } else {
-        while (x == ' ') {
-            ss >> x;
-        }
         if (x == ')') {
             y = NULL;
         } else {
@@ -231,12 +214,13 @@ bool isEqual_lisp(const lisp f, const lisp s) {
 
     if (isNull(f) && isNull(s)) {
         return true;
-    } else if (isAtom(f) && isAtom(s)) {
-            return true;
-    } else {
+    } else if ((!isAtom(f) && isAtom(s)) || (isAtom(f) && !isAtom(s)))
+      return false;
+     else if(!isAtom(f) && !isAtom(s)) {
         return isEqual_seg(f, s);
-    }
-    return false;
+    }else if(isAtom(f) && isAtom(s))
+      return true;
+    else return false;
 }
 
 bool isEqual_seg(const lisp f, const lisp s) {
@@ -255,63 +239,99 @@ int print(bool result, int sw_var){
   return sw_var = 4;
 }
 
- int main() {
+bool Check(istringstream &s) {
+	char x;
+	int countopen=0;
+	int countclose=0;
+	while (s >> x) {
+		if (x == '(')
+			countopen++;
+		if (x == ')')
+			countclose++;
+	}
+	if (countopen == countclose)
+		return true;
+	else
+		return false;
+}
 
-    lisp* s = new lisp;
-    lisp* f = new lisp;
-    string str1, str2;
-    int sw_var;
-    string file_name;
-    fstream file;
-    cout << "Menu"<< '\n';
-    cout << "0-exit from the program"<< '\n';
-    cout << "1-input a string from the keyboard"<< '\n';
-    cout << "2-input a line from a file" << '\n';
-    cin >> sw_var;
-    cin.ignore();
-    while(sw_var)
-    {
-        switch(sw_var)
-        {
-        case 1: {
-            cout << "Enter the first line :"<< '\n';
-            cin >> str1;
-            istringstream ss1(str1);
-            read_lisp(*f, ss1);
-            cout << "Enter the second line :"<< '\n';
-            cin >> str2;
-            istringstream ss2(str2);
-            read_lisp(*s, ss2);
-            sw_var=print(isEqual_lisp(*f, *s), sw_var); }
-            break;
-        case 2:{
-            cout << "Enter the name of the file:"<< '\n';
-            cin >> file_name;
-            cin.ignore();
-            file.open(file_name, fstream::in);
-            if(!file.is_open()){
-                cout << "Error opening file.\n";
-                sw_var = 4;
-              }
-            else{
-                getline(file, str1);
-                istringstream ss1(str1);
-                read_lisp(*f, ss1);
-                getline(file, str2);
-                istringstream ss2(str2);
-                read_lisp(*s, ss2);
-                sw_var=print( isEqual_lisp(*f, *s), sw_var);
-                file.close();
-              }}
-            break;
-        case 0:
-            break;
-        default: {
-            cout << "Enter again.\n";
-            cin >> sw_var;
-            cin.ignore();}
-            break;
-        }
-    }
-    return 0;
+string delet(string str){
+  int i=0;
+  while(str.length()>i){
+    if(str[i] == ' ')
+      str.erase(i,1);
+    else  i++;
   }
+  return str;
+}
+
+int main() {
+
+	lisp* s = new lisp;
+	lisp* f = new lisp;
+	string str1, str2;
+	int sw_var;
+	string file_name;
+	fstream file;
+	cout << "Menu" << '\n';
+	cout << "0-exit from the program" << '\n';
+	cout << "1-input a string from the keyboard" << '\n';
+	cout << "2-input a line from a file" << '\n';
+	cin >> sw_var;
+	cin.ignore();
+	while (sw_var)
+	{
+		switch (sw_var)
+		{
+		case 1: {
+			cout << "Enter the first line :" << '\n';
+			getline(cin, str1);
+      istringstream ss1(delet(str1));
+			if (!Check(ss1)) {
+				cout << "Error\n";
+        sw_var=4;
+				break;
+			}
+			read_lisp(*f, ss1);
+			cout << "Enter the second line :" << '\n';
+			getline(cin, str2);
+			istringstream ss2(delet(str2));
+			if (!Check(ss2)) {
+				cout << "Error\n";
+        sw_var=4;
+				break;
+			}
+			read_lisp(*s, ss2);
+			sw_var = print(isEqual_lisp(*f, *s), sw_var); }
+				break;
+		case 2: {
+			cout << "Enter the name of the file:" << '\n';
+			cin >> file_name;
+			cin.ignore();
+			file.open(file_name, fstream::in);
+			if (!file.is_open()) {
+				cout << "Error opening file.\n";
+				sw_var = 4;
+			}
+			else {
+				getline(file, str1);
+				istringstream ss1(delet(str1));
+				read_lisp(*f, ss1);
+				getline(file, str2);
+				istringstream ss2(delet(str2));
+				read_lisp(*s, ss2);
+				sw_var = print(isEqual_lisp(*f, *s), sw_var);
+				file.close();
+			}}
+				break;
+		case 0:
+			break;
+		default: {
+			cout << "Enter again.\n";
+			cin >> sw_var;
+			cin.ignore(); }
+				 break;
+		}
+	}
+	return 0;
+}
