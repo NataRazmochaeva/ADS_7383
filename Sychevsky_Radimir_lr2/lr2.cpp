ba7cb0d12ff0a9e7c4bc319c6d14cc5c14be0f28
Lucky324 <ridddik32424@gmail.com>
2018-11-03 23:05:54 +0300

    lr2 done

--- Sychevsky_Radimir_lr2/lr2.cpp
+++ Sychevsky_Radimir_lr2/lr2.cpp
@@ -2,20 +2,25 @@
 #include <fstream>
 #include <sstream>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
-struct elem{
-    int action;      // 1-OR, 2-AND, 3-XOR, 4-NOT
+struct atom;
+
+struct pairs{
+    atom* left;
+    atom* right;
+};
+
+struct atom{
+    bool isleft;
+    int action;     // 1-OR, 2-AND, 3-XOR, 4-NOT
+    bool flag;
     union{
         int a;
-        elem* next;
-    }un1;
-    union{
-        int b;
-        elem* next;
-    }un2;
+        pairs* next;
+    }un;
 };
-typedef elem *kek;
 
 void pr_menu(){
 	cout << "Выберите действие:" << endl;
@@ -23,94 +28,106 @@ void pr_menu(){
     cout << "2 - ввод из файла" << endl;
     cout << "0 - выход" << endl;
 }
-
+    
 int find_arg(string str1){
-    if(str1[1] == 'O')
+    if(str1[0] == 'O')
         return 1;
-    if(str1[1] == 'A')
+    if(str1[0] == 'A')
         return 2;
-    if(str1[1] == 'X')
+    if(str1[0] == 'X')
         return 3;
-    if(str1[1] == 'N')
+    if(str1[0] == 'N')
         return 4;
     return 0;
 }
 
-int full_elem(elem* expression, string str1, string str2){ // OR, AND, XOR, NOT
-    expression->action = find_arg(str1);
-    switch(expression->action){                            //>
-        case 1:                                            // >
-            for(int i = 4; i < str1.length()-1; i++)       //  >
-                str1[i-4] = str1[i];                       //   >
-            str1[str1.length()-1] = ' ';                   //    >
-            break;                                         //     > стирание операции
-        default:                                           //    >
-            for(int i = 5; i < str1.length()-1; i++)       //   >
-                str1[i-5] = str1[i];                       //  >
-            str1[str1.length()-1] = ' ';                   // >
-    }                                                      //>
-    if(str1[0] == '('){                                   
-        expression->un1.next = new elem;                   
-        full_elem(expression->un1.next, str1, str2);       
-        int k = 1;                                        
-        int count = 1;                                     
-        while(count){                                      
-            if(str1[k] == '(')                            
-                count++;                                  
-            if(str1[k] == ')')                            
-                count--;                                  
-            k++;                                          
-        }                                                 
-        for(int i = (k+1); i < str1.length();i++)         
-            str1[i-k-1] = str1[i];                         
-    } else {        
-        for(int i = 0; i < str2.length()-1; i++)           //ввод первой переменной
-            if(str1[0] == str2[i])
-                expression->un1.a = str2[i+2] - 48;
-        for(int i = 2; i < str1.length()-1; i++)
-            str1[i-2] = str1[i];
+int full_elem(pairs* knot, string str1, string str2){ // OR, AND, XOR, NOT
+    int FFF = 0;
+    int F_LOC = 0;
+    knot->left = new atom;
+    knot->left->isleft = true;
+    knot->left->action = find_arg(str1);
+    string tmp;
+    switch(knot->left->action){
+        case 1:
+            F_LOC = 3;
+            tmp = str1.substr(3);
+            str1 = tmp;
+            break;
+        default:
+            F_LOC = 4;
+            tmp = str1.substr(4);
+            str1 = tmp;
     }
-    if(expression->action != 4){
-        if(str1[0] == '('){
-            expression->un2.next = new elem;
-            full_elem(expression->un2.next, str1, str2);
-        } else {                                           //вторая переменная
-            for(int i = 0; i < str2.length()-1; i++)       //ввод второй переменной
-                if(str1[0] == str2[i])
-                    expression->un2.b = str2[i+2] - 48;
+    if((str1[0] > 64) && (str1[0] < 91)){
+        knot->left->flag = true;
+        knot->left->un.next = new pairs;
+        FFF += full_elem(knot->left->un.next, str1, str2);
+        if(knot->left->action != 4){
+            tmp = str1.substr(FFF);
+            str1 = tmp;
         }
     } else {
-        expression->un2.next = NULL;
+        for(int i = 0; i < str2.length(); i++)
+            if(str1[0] == str2[i])
+                knot->left->un.a = str2[i+2] - 48;
+        if(knot->left->action != 4){
+            tmp = str1.substr(2);
+            str1 = tmp;
+        }
+        FFF += 2;
     }
-    return 0;
+    if(knot->left->action != 4){
+        if((str1[0] > 64) && (str1[0] < 91)){
+            knot->right = new atom;
+            knot->right->isleft = false;
+            knot->right->flag = true;
+            knot->right->un.next = new pairs;
+            FFF += full_elem(knot->right->un.next, str1, str2);
+        } else {
+            knot->right = new atom;
+            for(int i = 0; i < str2.length(); i++)
+                if(str1[0] == str2[i]){
+                    knot->right->un.a = str2[i+2] - 48;
+                    knot->right->flag = false;
+                }
+            FFF += 2;
+        }
+    } else {
+        knot->right = NULL;
+    }
+    FFF += F_LOC;
+    return FFF;
 }
 
-int find_ans(elem* expression){
-    if((expression->un1.a != 1) && (expression->un1.a != 0)){
-        int tmp1 = find_ans(expression->un1.next);
-        delete(expression->un1.next);
-        expression->un1.next = NULL;
-        expression->un1.a = tmp1;
+int find_ans(pairs* knot){
+    if(knot->left->flag){
+        int tmp1 = find_ans(knot->left->un.next);
+        delete(knot->left->un.next);
+        knot->left->un.next = NULL;
+        knot->left->un.a = tmp1;
+        knot->left->flag = false;
     }
-    if(expression->action != 4)
-        if((expression->un2.b != 1) && (expression->un2.b != 0)){
-            int tmp2 = find_ans(expression->un2.next);
-            delete(expression->un2.next);
-            expression->un2.next = NULL;
-            expression->un2.b = tmp2;
+    if(knot->left->action != 4)
+        if(knot->right->flag){
+            int tmp2 = find_ans(knot->right->un.next);
+            delete(knot->right->un.next);
+            knot->right->un.next = NULL;
+            knot->right->un.a = tmp2;
+            knot->right->flag = false;
         }
-    switch(expression->action){
+    switch(knot->left->action){
         case 1:
-            return (expression->un1.a | expression->un2.b);
+            return (knot->left->un.a | knot->right->un.a);
             break;
         case 2:
-            return (expression->un1.a & expression->un2.b);
+            return (knot->left->un.a & knot->right->un.a);
             break;
         case 3:
-            return (expression->un1.a ^ expression->un2.b);
+            return (knot->left->un.a ^ knot->right->un.a);
             break;
         case 4:
-            return (!expression->un1.a);
+            return (!knot->left->un.a);
             break;
     }
 }
@@ -123,10 +140,10 @@ int work_with_console(){
     cout << "Введите значеия переменных" << endl;
     string str2;
     getline(cin, str2);
-    elem* expression = new elem;
-    full_elem(expression, str1, str2);
-    cout << find_ans(expression) << endl;
-    delete(expression);
+    pairs* knot = new pairs;
+    full_elem(knot, str1, str2);
+    cout << find_ans(knot) << endl;
+    delete(knot);
     return 0;
 }
 
@@ -145,10 +162,10 @@ int work_with_file(){
     getline(f, str1);
     getline(f, str2);
     cout << str1 << endl << str2 << endl;
-    elem* expression = new elem;
-    full_elem(expression, str1, str2);
-    cout << find_ans(expression) << endl;
-    delete(expression);
+    pairs* knot = new pairs;
+    full_elem(knot, str1, str2);
+    cout << find_ans(knot) << endl;
+    delete(knot);
     return 0;
 }
 
