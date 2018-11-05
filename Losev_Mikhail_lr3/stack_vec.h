c9c424213064fca81a8a216b0ade079755c6dbd4
LosevMikhail <36499408+LosevMikhail@users.noreply.github.com>
2018-11-05 22:21:55 +0300

    Add files via upload

---
+++ Losev_Mikhail_lr3/stack_vec.h
@@ -0,0 +1,76 @@
+#ifndef STACK_VEC_H
+#define STACK_VEC_H
+
+template <class base>
+class Stack
+{
+    private:
+        base * arr;
+        int size;
+        int len;
+    public:
+        Stack()
+        {
+            len = 0;
+            size = 5;
+            arr = new base[size];
+        }
+
+
+        ~Stack()
+        {
+            delete [] arr;
+        }
+
+
+        int lenth()
+        {
+            return len;
+        }
+
+
+        void push(base x)
+        {
+            if (len == size) 
+            new_size(size<<1);
+            arr[len++] = x;
+            //cout << "Push -> " << x << endl;
+        }
+
+        base pop()
+        {
+            if (len == 0) 
+            return arr[len];
+            return arr[--len];
+        }
+
+        base top()
+        {
+            return arr[len];
+        }
+
+        void destroy()
+        {
+            delete [] arr;
+        }
+
+        bool isEmpty()
+        {
+            return len == 0;
+        }
+
+        void new_size(int s)
+        {
+            if (s <= 0) 
+                return;
+            base *new_arr = new base[s];
+            for (int i = 0; i < s; i++) 
+                new_arr[i] = arr[i];
+            delete[] arr;
+            size = s;
+            arr = new_arr;
+            //cout<<"NEW SIZE "<<size<<endl;
+        }
+};
+
+#endif
