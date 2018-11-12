e8cb53109a2b5d71c1dbf588533e640744063895
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2018-11-12 21:26:34 +0300

    Add files via upload

--- Levkovich_Dmitry_lr4/main.cpp
+++ Levkovich_Dmitry_lr4/main.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 #include <cstring>
 #include <exception>
+#include <windows.h>
 
- using namespace std ;
- typedef char base;
+using namespace std ;
+
+typedef char base;
     class node {
     public:
         base info;
@@ -17,56 +19,69 @@
                 lt = nullptr;
                 rt = nullptr;
             }
-	~node(){
-		cout << "<3 " << info << endl;
-		delete lt;
-		delete rt;
-	}
     };
     typedef node *binTree; // "представитель" бинарного дерева
     binTree Create(void);
-    bool isNull(binTree);
+    bool isnullptr(binTree);
     base RootBT (binTree); // для непустого бин.дерева
     binTree Left (binTree);// для непустого бин.дерева
     binTree Right (binTree);// для непустого бин.дерева
     binTree ConsBT(const base &x, const binTree &lst,  const binTree &rst);
     void destroy (binTree&);
-     typedef binTree Form;
+
+    typedef binTree Form;
     Form readForm(std::istream &in);
     void writeForm(std::ostream &out, const Form f);
     Form deriviate(const Form f, char var);
-     binTree Create()
+
+
+    binTree Create()
     {
         return nullptr;
     }
-     bool isNull(binTree b)
+
+    bool isnullptr(binTree b)
     {
         return (b == nullptr);
     }
-     base RootBT (binTree b)                     // для непустого бин.дерева
-    {   if (b == nullptr) { cerr << "Error: RootBT(null) \n"; exit(1); }
+
+    base RootBT (binTree b)                     // для непустого бин.дерева
+    {   if (b == nullptr) { cerr << "Error: RootBT(nullptr) \n"; exit(1); }
         else return b->info;
     }
-     binTree Left (binTree b)            // для непустого бин.дерева
-    {   if (b == nullptr) { cerr << "Error: Left(null) \n"; exit(1); }
+
+    binTree Left (binTree b)            // для непустого бин.дерева
+    {   if (b == nullptr) { cerr << "Error: Left(nullptr) \n"; exit(1); }
         else return b ->lt;
     }
-     binTree Right (binTree b)           // для непустого бин.дерева
-    {   if (b == nullptr) { cerr << "Error: Right(null) \n"; exit(1); }
+
+    binTree Right (binTree b)           // для непустого бин.дерева
+    {   if (b == nullptr) { cerr << "Error: Right(nullptr) \n"; exit(1); }
         else return b->rt;
     }
-     binTree ConsBT(const base &x, const binTree &lst, const binTree &rst)
+
+    binTree ConsBT(const base &x, const binTree &lst, const binTree &rst)
     {   binTree p;
         p = new node;
-
+        if ( p != nullptr) {
             p ->info = x;
             p ->lt = lst;
             p ->rt = rst;
             return p;
-        
+        }
+        else {cerr << "Memory not enough\n"; exit(1);}
     }
 //-------------------------------------
-     bool isSign(char c){
+    node* Delete(node* p){
+        if (left)
+            delete p->lt;
+        if (right)
+            delete p->rt;
+        delete p;
+        return p = NULL;
+    }
+
+    bool isSign(char c){
         return c=='+'||c=='-'||c=='*';
     }
     bool isTerminal(char c){
@@ -74,7 +89,7 @@
     }
     Form readForm(istream &in){
         if(isTerminal(in.peek())){
-            return ConsBT(in.get(), nullptr, nullptr);
+            return ConsBT(in.get(),nullptr,nullptr);
         }
         if(in.peek()=='('){
             in.get();
@@ -89,12 +104,12 @@
         throw "expected term or '('\n";
     }
     void writeForm(ostream &out, const Form f){
-        if(isNull(f))throw "Error, empty formula\n";
-        if(isNull(Right(f))&&isNull(Left(f))){
+        if(isnullptr(f))throw "Error, empty formula\n";
+        if(isnullptr(Right(f))&&isnullptr(Left(f))){
             out<<RootBT(f);
             return;
         }
-        if(isNull(Right(f))||isNull(Left(f)))
+        if(isnullptr(Right(f))||isnullptr(Left(f)))
             throw "Knot should have Left and Right\n";
         out<<'(';
         writeForm(out,Left(f));
@@ -102,9 +117,10 @@
         writeForm(out,Right(f));
         out<<')';
     }
-     Form deriviate(const Form f, char var){
+
+    Form deriviate(const Form f, char var){
         if(var==RootBT(f)||isdigit(RootBT(f))||isalpha(RootBT(f))){
-            if(!isNull(Right(f))||!isNull(Left(f)))
+            if(!isnullptr(Right(f))||!isnullptr(Left(f)))
                 throw "variable or const shouldn't be in root\n";
             if(var==RootBT(f))             // x'=1
                 return ConsBT('1',nullptr,nullptr);
@@ -117,7 +133,19 @@
                           deriviate(Right(f),var)
                    );
         if(RootBT(f)=='*')                 // (f(x)*g(x))'=f'(x)*g(x)+f(x)*g'(x)
-            return ConsBT('+',ConsBT('*',deriviate(Left(f),var),Right(f)),ConsBT('*',Left(f),deriviate(Right(f),var)));
+            return ConsBT(
+                          '+',
+                          ConsBT(
+                              '*',
+                              deriviate(Left(f),var),
+                              Right(f)
+                          ),
+                          ConsBT(
+                              '*',
+                              Left(f),
+                              deriviate(Right(f),var)
+                          )
+                   );
         throw "unknown char\n";
     }
     int calc(Form f){
@@ -127,7 +155,8 @@
         if((f->info=='+')||(f->info=='-')||(f->info=='*')){
             x=calc(f->lt);
             y=calc(f->rt);
-             if (f->info=='+'){
+
+            if (f->info=='+'){
                 cout<<"you are here: "<<x<<'+'<<y<<endl;
                 z=x+y;
             }
@@ -156,46 +185,77 @@
                 cout<<f->info<<endl;
                 print(f->lt, l+1);
     }
- int main (){
 
-    Form f;
-    Form fPrime;
-    int run = 1;
-    string k;
-    int m;
-    while(run){
-    cout<<"enter the variable for derivative"<<endl;
-    char c;
-    cin>>c;
-    cin.ignore();
-    try{
-        cout<<"Source formula:";
-        writeForm(cout, (f=readForm(cin)));
-        cout<<endl;
-        try{
-        cout<<"expression value: ";
-        cout<<calc(f)<<endl;
-        }
-        catch(const char* e){
-            cout<<e;
-        }
-        cout<<"graphical representation:"<<endl;
-        cout<<endl;
-        print(f, 0);
-        cout<<"derivative for "<<c<<endl;
-        writeForm(cout, (fPrime=deriviate(f,c)));
-        cout<<endl;
+    int main(){
+        Form f = nullptr;
+        Form fPrime = nullptr;
+        char c;
+        int key = 0;
+        string str;
+        filebuf file;
+        string file_name;
+        stringbuf exp;
+        string temp_str;
+        char forSwitch;
+        while(1){
+           cout<<"Press 1 to read from console, press 2 to read from 1.txt file, press 0 to exit."<<endl;
+           cin >> forSwitch;
+           getchar();
+           switch (forSwitch) {
+               case '2':{
+                   ifstream infile("1.txt");
+                   if(!infile){
+                         cout<<"There is no file"<<endl;
+                         continue;
+                         }
+                   getline(infile, temp_str);
+                   istream is(&exp);
+                   exp.str(temp_str);
+
+                   break;
+               }
+               case '1':{
+                       cout<<"Enter the variable for derivative"<<endl;
+                       cin>>c;
+                       try{
+                       cin.ignore();
+                       cout<<"Enter the expression"<<endl;
+                       getline(cin, temp_str);
+                       istream is(&exp);
+                       exp.str(temp_str);
+                       writeForm(cout, f=readForm(is));
+                       cout<<endl;
+                       try{
+                       cout<<"expression value: ";
+                       cout<<calc(f)<<endl;
+                       }
+                       catch(const char* e){
+                           cout<<e;
+                       }
+
+                       print(f, 0);
+                       writeForm(cout, (fPrime=deriviate(f, c)));
+                       cout<<endl;
+                       break;
+                       }
+                       catch(const char* t){
+                           cout<<t;
+                       }
+               }
+               case '0':{
+                       return 0;
+               }
+               default:{
+                       cout<<"Incorrect input"<<endl;
+                       break;
+               }
+
+           }
+
+           f=Delete(f);
+           fPrime=Delete(fPrime);
+       str.clear();
+       }
+
     }
-    catch(const char* t){
-        cout<<t;
-    }
-	delete f;
-	delete fPrime;
-    cout<<"Continue? Press 1. Else press 0"<<endl;
-        cin>>m;
-        cin.ignore();
-        if(m==0)
-            return 0;
-    }
-    return 0;
-}
+
