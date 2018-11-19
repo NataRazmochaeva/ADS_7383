7f0177a2a955a6bf84f7e2474d4dd5a59dfefca7
yulyakhanovagit <yulya_khanova@mail.ru>
2018-11-20 03:56:06 +0400

    Khanova Yulya lr4 done

---
+++ Khanova_Yulya_lr4/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "binTree.h"
+#include <string>
+using namespace std;
+using namespace binTree_modul;
+void displayBT(int b, int n)
+{	// n  － уровень узла
+	if (b != 0) {
+		cout << ' ' << RootBT(b);
+		if (!isNull(Right(b))) { displayBT(Right(b), n + 1); }
+		else cout << endl; // вниз
+		if (!isNull(Left(b))) {
+			for (int i = 1; i <= n; i++) cout << "  "; // вправо
+			displayBT(Left(b), n + 1);
+		}
+	}
+	else {};
+}
+
+int build(string a, string b, int x) {
+	int k;
+	if (a == "") return 0;
+	if (a.length() == 1) {
+		k = ConsBT(a[0], 0, 0);
+		return k;
+	}
+	if (x < 0) return 0;
+	int flag = a.find(b[x]);
+	if (flag < 0) return 0;
+	k = ConsBT(b[x], build(a.substr(0, flag), b.substr(0, flag), flag-1), build(a.substr(flag + 1), b.substr(flag, x-1), x-1-flag));
+	return k;
+}
+int main() {
+
+	ifstream fin("input.txt");
+	if (!fin) { cout << "File not open for reading!\n"; return 1; }
+	string str1, str2;
+	cout << "Считывается ЛКП запись..." << endl;
+	getline(fin, str1, '\n');
+	cout << "Считывается ЛПК запись..." << endl;
+	getline(fin, str2, '\n');
+	int MyTree = build(str1, str2, str2.length() - 1);
+	displayBT(MyTree, 1);
+	system("pause > nul");
+	return 0;
+}
---
+++ Khanova_Yulya_lr4/binTree.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include "binTree.h"
+using namespace std;
+
+namespace binTree_modul
+{
+	node* Mem = new node[100];
+	bool isNull(int b){
+		return (b == 0);
+	}
+	char RootBT(int b){
+		if (b == 0) {
+			cerr << "Error: RootBT(null) \n";
+			system("pause > nul");
+		}
+		else return Mem[b].info;
+	}
+	int Left(int b){
+		if (b == 0) {
+			cerr << "Error: Left(null) \n";
+			system("pause > nul");
+		}
+		else return Mem[b].lt;
+	}
+	int Right(int b)	{
+		if (b == 0) {
+			cerr << "Error: Right(null) \n";
+			system("pause > nul");
+		}
+		else return Mem[b].rt;
+	}
+
+	int freeND(){
+		for(int i=1; i<100; i++){
+			if(Mem[i].info == 0) return i;
+		}
+		return -1;
+	}
+	int ConsBT(const char &x, int lt, int rt){
+		int k = freeND();
+		if (k > 0) {
+			Mem[k].info = x;
+			Mem[k].lt = lt;
+			Mem[k].rt = rt;
+			return k;
+		}
+		else { cerr << "Memory not enough\n"; exit(1); }
+	}
+}
